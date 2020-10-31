7d0b6313d3d33d198f830060b48552f89ea826fc
James Liu <james_liu@fastmail.com>
2020-10-31 14:55:06 +1100

    feat: execute expressions

--- eval.py
+++ eval.py
@@ -1,13 +1,39 @@
+import socket
 from code import compile_command
+
+ENCODING = 'utf-8'
+PORT = 1337
 
 _globals = {}
 _locals = {}
 
-while True:
-  try:
-    obj = compile_command(input(">>> "))
-    if obj is None:
-      print("Incomplete expression")
-    exec(obj, _globals, _locals)
-  except (SyntaxError, ValueError, OverflowError) as e:
-    print(e)
+def evlauate(expr):
+    try:
+        obj = compile_command(expr)
+        if obj is None:
+            print("Incomplete expression")
+        exec(obj, _globals, _locals)
+    except (SyntaxError, ValueError, OverflowError) as e:
+        print(e)
+
+def process_request(sock):
+    (conn, addr) = sock.accept()
+    with conn:
+        print('Connected from ', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                sock.close()
+                break
+            expr = data.decode(ENCODING)
+            result = evlauate(expr)
+            conn.sendall(result.encode(ENCODING))
+
+
+if __name__ == '__main__':
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind((socket.gethostname(), PORT))
+    serversocket.listen(5)
+
+    while True:
+        process_request(serversocket)
