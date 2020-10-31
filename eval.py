625dd337f921df81b39bab2e8abf3753aaf041a1
James Liu <james_liu@fastmail.com>
2020-10-31 18:11:45 +1100

    feat: get result of last expression

--- eval.py
+++ eval.py
@@ -3,30 +3,38 @@ import json
 from code import InteractiveInterpreter
 
 class SocketInterpreter(InteractiveInterpreter):
-    KEY_LAST_EXPR_RESULT = "_LAST_EXPR_RESULT"
-
     def __init__(self, encoding):
         InteractiveInterpreter.__init__(self, {})
         self.encoding = encoding
-        self.last_expr_result = None
+        self.last_code = None
 
     def write(self, data):
         print("overloaded")
         print(data)
 
+    def runcode(self, code):
+        """Override
+        """
+        try:
+            exec(code, self.locals)
+            self.last_code = code
+        except SystemExit:
+            raise
+        except:
+            self.showtraceback()
+
     def evaluate(self, source):
-        ret = self.runsource(f"{SocketInterpreter.KEY_LAST_EXPR_RESULT} = {source}")
-        self.last_expr_result = self.locals["_LAST_EXPR_RESULT"]
+        ret = self.runsource(source)
         return ret
 
     def trimmed_locals(self):
-        return {k: v for k, v in self.locals.items() if k not in ["__builtins__", SocketInterpreter.KEY_LAST_EXPR_RESULT]}
+        return {k: v for k, v in self.locals.items() if k not in ["__builtins__"]}
     
     def serialised_locals(self):
         return json.dumps(self.trimmed_locals()).encode(self.encoding)
 
     def get_last_expr_result(self):
-        return self.last_expr_result
+        return self.locals[self.last_code.co_names[-1]]
 
 def process_request(sock, interp):
     (conn, addr) = sock.accept()
@@ -39,8 +47,8 @@ def process_request(sock, interp):
                 break
             expr = data.decode(ENCODING)
             interp.evaluate(expr)
-            conn.sendall(interp.serialised_locals())
             print(interp.get_last_expr_result())
+            conn.sendall(interp.serialised_locals())
 
 
 if __name__ == '__main__':
