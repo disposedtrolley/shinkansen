ede504c290b71d4aa5fe232ddc155b694fde8792
James Liu <james_liu@fastmail.com>
2020-10-31 17:47:42 +1100

    feat: save result of most recent expression

--- eval.py
+++ eval.py
@@ -3,19 +3,30 @@ import json
 from code import InteractiveInterpreter
 
 class SocketInterpreter(InteractiveInterpreter):
+    KEY_LAST_EXPR_RESULT = "_LAST_EXPR_RESULT"
+
     def __init__(self, encoding):
         InteractiveInterpreter.__init__(self, {})
         self.encoding = encoding
+        self.last_expr_result = None
 
     def write(self, data):
         print("overloaded")
         print(data)
 
+    def evaluate(self, source):
+        ret = self.runsource(f"{SocketInterpreter.KEY_LAST_EXPR_RESULT} = {source}")
+        self.last_expr_result = self.locals["_LAST_EXPR_RESULT"]
+        return ret
+
     def trimmed_locals(self):
-        return {k: v for k, v in self.locals.items() if k != "__builtins__"}
+        return {k: v for k, v in self.locals.items() if k not in ["__builtins__", SocketInterpreter.KEY_LAST_EXPR_RESULT]}
     
     def serialised_locals(self):
         return json.dumps(self.trimmed_locals()).encode(self.encoding)
+
+    def get_last_expr_result(self):
+        return self.last_expr_result
 
 def process_request(sock, interp):
     (conn, addr) = sock.accept()
@@ -27,8 +38,9 @@ def process_request(sock, interp):
                 sock.close()
                 break
             expr = data.decode(ENCODING)
-            interp.runsource(expr)
+            interp.evaluate(expr)
             conn.sendall(interp.serialised_locals())
+            print(interp.get_last_expr_result())
 
 
 if __name__ == '__main__':
