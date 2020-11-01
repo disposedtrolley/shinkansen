2ecd89b0f6d5163b51f5b6512ec0c192ec42f462
James Liu <james_liu@fastmail.com>
2020-11-01 11:40:19 +1100

    docs: add docstrings and comments for server

--- src/interpreters/python/server.py
+++ src/interpreters/python/server.py
@@ -16,12 +16,15 @@ class SocketInterpreter(InteractiveInterpreter):
         self._incomplete = False
 
     def write(self, data):
-        """Override
+        """Overrides the InteractiveInterpreter's write() method to redirect
+        syntax errors into _last_expr_error.
         """
         self._last_expr_error = data
 
     def runcode(self, code):
-        """Override
+        """Overrides the InteractiveInterpreter's runcode() method to intercept
+        the compiled `code` object, execute it, and attempt to extract the
+        results of running the expression.
         """
         try:
             buf = io.StringIO()
@@ -37,14 +40,23 @@ class SocketInterpreter(InteractiveInterpreter):
             self.showtraceback()
 
     def _reset_expr_state(self):
+        """Resets the results of execution.
+        """
         self._last_expr_result = None
         self._last_expr_error = None
         self._incomplete = False
 
     def _trimmed_locals(self):
+        """Trims the locals dict to omit non-user created symbols.
+        """
         return {k: v for k, v in self.locals.items() if k not in ["__builtins__", "__doc__", "__name__"]}
 
     def _serialisable_locals(self):
+        """Returns a filtered dict of locals which are serialisable.
+        Serialisable locals are those with translations into JSON (i.e. most
+        primitives). Other symbols like class and function definitions are
+        converted to their __str__ representation.
+        """
         serialisable = {}
         for k, v in self._trimmed_locals().items():
             try:
@@ -57,6 +69,9 @@ class SocketInterpreter(InteractiveInterpreter):
         return serialisable
     
     def _result_from_code(self, code):
+        """Attempts to return the value gained from executing the expression
+        in `code`.
+        """
         if len(code.co_names) == 0:
             return None
 
@@ -67,16 +82,26 @@ class SocketInterpreter(InteractiveInterpreter):
         return self._serialisable_locals()[key]
     
     def _result_from_stdout(self, buf):
+        """Returns anything captured from STDOUT during the execution of an
+        expression.
+        """
         return buf.getvalue().rstrip("\n")
 
     def evaluate(self, source):
+        """Evaluates the expression in `source`.
+        """
         self._reset_expr_state()
         ret = self.runsource(source)
         if ret is True:
+            # runsource() returns True if the expression is incomplete, i.e.
+            # `source` is a class or function signature and a body must be
+            # supplied.
             self._incomplete = True
         return ret
 
     def results(self):
+        """Returns JSON serialised results of calling `evaluate().
+        """
         return json.dumps({
             "locals": self._serialisable_locals(),
             "last_expr_result": self._last_expr_result,
@@ -86,27 +111,31 @@ class SocketInterpreter(InteractiveInterpreter):
 
 
 if __name__ == '__main__':
+    # Listen for client connections.
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.bind((socket.gethostname(), PORT))
-    serversocket.listen(5)
-
+    serversocket.listen(5)  # 5 is arbitrary
     print("Server listening on", PORT)
 
     while True:
         try:
+            # Accept a connection and create a new SocketInterpreter.
             (conn, addr) = serversocket.accept()
             print(addr, 'connected')
             interp = SocketInterpreter()
             while True:
                 data = conn.recv(1024)
                 if not data:
+                    # We arrive here if the client has disconnected.
                     print(addr, 'disconnected')
                     conn.close()
                     break
+                # Decode, evaluate, and return the results.
                 expr = data.decode(ENCODING)
                 interp.evaluate(expr)
                 conn.sendall(interp.results().encode(ENCODING))
         except KeyboardInterrupt:
+            # Graceful shutdown.
             print('Qutting...')
             serversocket.close()
             sys.exit(0)
