99ba00c90062929c22be6c05263f854c5aabdb43
James Liu <james_liu@fastmail.com>
2020-11-01 11:47:31 +1100

    refactor: format and clean up client

--- src/interpreters/python/client.js
+++ src/interpreters/python/client.js
@@ -1,42 +1,45 @@
-const net = require('net');
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
+import { createConnection } from 'net';
+import { createInterface } from 'readline';
 
-let promptNormal = ">>>"
-let promptIncomplete = "..."
-let buf = ""
-let incomplete = false
-let promptCurrent = promptNormal
+const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-const client = net.createConnection({ port: 1337 }, () => {
-    console.log('connected!')
-    promptExpr(client)
-})
+const promptNormal = ">>>";
+const promptIncomplete = "...";
+
+let buf = "";
+let incomplete = false;
+let promptCurrent = promptNormal;
+
+const client = createConnection({ port: 1337 }, () => {
+    console.log('connected!');
+    promptExpr(client);
+});
 
 client.on('data', data => {
-    j = JSON.parse(data.toString())
-    incomplete = j.incomplete
-    console.log(j)
-    promptExpr(client)
-})
+    j = JSON.parse(data.toString());
+    incomplete = j.incomplete;
+    console.log(j);
+    promptExpr(client);
+});
 
 client.on('end', () => {
-    console.log('disconnected')
-})
+    console.log('disconnected');
+});
 
 const promptExpr = (client) => {
-    promptCurrent = incomplete ? promptIncomplete : promptNormal
+    promptCurrent = incomplete ? promptIncomplete : promptNormal;
 
     readline.question(`\n${promptCurrent} `, expr => {
         if (incomplete) {
-            buf = `${buf}\n${expr}`
+            buf = `${buf}\n${expr}`;
         } else {
-            buf = expr
+            buf = expr;
         }
 
-        console.log(buf)
-        client.write(buf)
-    })
-}
+        console.log(buf);
+        client.write(buf);
+    });
+};
