8496695ccdce07945a4fbcf7dc3afc615da03690
James Liu <james_liu@fastmail.com>
2020-11-03 10:26:40 +1100

    refactor: wrap state into a State object

--- src/extension.ts
+++ src/extension.ts
@@ -2,16 +2,26 @@ import * as vscode from 'vscode';
 import { createConnection } from 'net';
 import { PythonSymbol, PythonSymbolProvider } from './symbols/python';
 
-let currentEditor: vscode.TextEditor;
-let symbolProvider: PythonSymbolProvider;
-let expressionBuffer: string;
-let lastExpression: string;
-let isIncomplete: boolean = false;
+interface State {
+    currentEditor?: vscode.TextEditor;
+    symbolProvider?: PythonSymbolProvider;
+    expressionBuffer: string;
+    lastExpression: string;
+    isIncomplete: boolean;
+}
+
+let state: State = {
+    currentEditor: undefined,
+    symbolProvider: undefined,
+    expressionBuffer: "",
+    lastExpression: "",
+    isIncomplete: false
+};
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('"shinkansen" is now active!');
 
-    symbolProvider = new PythonSymbolProvider();
+    state.symbolProvider = new PythonSymbolProvider();
 
     const interpreterClient = createConnection({ port: 1337 }, () => {
         console.log('connected!');
@@ -24,22 +34,21 @@ export function activate(context: vscode.ExtensionContext) {
     interpreterClient.on('data', (data: string) => {
         const j = JSON.parse(data.toString());
         console.log(j);
+        state.isIncomplete = j.incomplete;
 
-        isIncomplete = j.incomplete;
-
-        currentEditor.edit(e => {
+        state.currentEditor?.edit(e => {
             e.insert(
                 new vscode.Position(
-                    currentEditor.selection.active.line,
-                    currentEditor.selection.active.character + 2),
+                    state.currentEditor!.selection.active.line,
+                    state.currentEditor!.selection.active.character + 2),
                 `    # => ${j.last_expr_result}`);
         });
     });
 
     let disposable = vscode.commands.registerCommand('shinkansen.evaluate', () => {
-        currentEditor = vscode.window.activeTextEditor!;
+        state.currentEditor = vscode.window.activeTextEditor!;
 
-        symbolProvider.symbolAtPoint(currentEditor.selection.active, currentEditor.document)
+        state.symbolProvider!.symbolAtPoint(state.currentEditor.selection.active, state.currentEditor.document)
             .then((symbol: PythonSymbol | null) => {
                 let expr;
                 if (symbol) {
@@ -47,26 +56,26 @@ export function activate(context: vscode.ExtensionContext) {
                     console.log(symbol);
                     expr = symbol.expression().body;
                 } else {
-                    expr = currentEditor.document.lineAt(currentEditor.selection.active.line).text;
+                    expr = state.currentEditor!.document.lineAt(state.currentEditor!.selection.active.line).text;
                 }
 
                 // If the last expression was incomplete, this one needs to be
                 // appended to the buffer.
-                if (isIncomplete) {
+                if (state.isIncomplete) {
                     // We can end an incomplete expression by evaluating the exact same
                     // line again.
                     // `startsWith()` because the expression now contains the result of
                     // evaluation.
-                    expressionBuffer =
-                        expr.startsWith(lastExpression) ? 
-                        `${expressionBuffer}\n\n` : `${expressionBuffer}\n${expr}`;
+                    state.expressionBuffer =
+                        expr.startsWith(state.lastExpression) ?
+                            `${state.expressionBuffer}\n\n` : `${state.expressionBuffer}\n${expr}`;
                 } else {
                     // Reset
-                    expressionBuffer = expr;
+                    state.expressionBuffer = expr;
                 }
 
-                lastExpression = expr;
-                interpreterClient.write(expressionBuffer);
+                state.lastExpression = expr;
+                interpreterClient.write(state.expressionBuffer);
             });
 
     });
